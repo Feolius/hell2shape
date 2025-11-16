ffe1b65eb26ee24014100d812b1f06c74f2e1e1f
ivanstrygin <feolius@gmail.com>
2025-11-16 18:48:53 +0300

    Rewrite lexer using singular regexp approach.

--- src/Lexer/Lexer.php
+++ src/Lexer/Lexer.php
@@ -4,14 +4,6 @@ namespace App\Lexer;
 
 final class Lexer
 {
-    public const string SPACE_CHAR = ' ';
-
-    public const string NEW_LINE_CHAR = "\n";
-
-    public const string OPEN_BRACE_CHAR = "{";
-
-    public const string CLOSE_BRACE_CHAR = "}";
-
     public const string T_ARRAY = 'array';
 
     public const string T_ARROW = 'arrow';
@@ -19,6 +11,10 @@ final class Lexer
     public const string T_STRING_KEY = 'string_key';
 
     public const string T_INT_KEY = 'int_key';
+
+    public const string T_PROTECTED_PROP = 'protected';
+
+    public const string T_PRIVATE_PROP = 'private';
 
     public const string T_OPEN_BRACE = 'open_brace';
 
@@ -30,15 +26,21 @@ final class Lexer
 
     public const string T_BOOL = 'bool';
 
-    public const string T_DOUBLE = 'double';
+    public const string T_FLOAT = 'float';
 
     public const string T_NULL = 'null';
 
-    public const string T_OBJECT = 'object';
+    public const string T_UNINITIALIZED = 'uninitialized';
+
+    public const string T_STD_OBJECT = 'std_object';
 
     public const string T_RESOURCE = 'resource';
 
-    public const string T_CLASS = 'class';
+    public const string T_OBJECT = 'object';
+
+    public const string T_NEWLINE = 'newline';
+
+    public const string T_WS = 'ws';
 
     /**
      * @return list<Token>
@@ -49,70 +51,62 @@ final class Lexer
         $tokens = [];
 
         $patterns = [
-            self::T_STRING => '/^string\(\d+\) \"[^\"]*\"/',
-            self::T_INT => '/^int\(\d+\)/',
-            self::T_BOOL => '/^bool\((?:true|false)\)/',
-            self::T_DOUBLE => '/^double\(\d+\.\d+\)/',
-            self::T_NULL => '/^NULL/',
-            self::T_ARRAY => '/^array\(\d+\)/',
-            self::T_ARROW => '/^=>/',
-            self::T_STRING_KEY => '/^\'[a-zA-Z0-9_]+\'/',
-            self::T_INT_KEY => '/^\[\d+\]/',
-            self::T_OPEN_BRACE => '/^\{/',
-            self::T_CLOSE_BRACE => '/^\}/',
-            self::T_OBJECT => '/^class stdClass#\d+ \(\d+\)/',
-            self::T_RESOURCE => '/^resource\(\d+\) of type \(\w+\)/',
-            self::T_CLASS => '/^class [\w\\\\]+#\d+ \(\d+\)/u',
+            self::T_STRING => 'string\(\d+\) \"[^\"]*\"',
+            self::T_INT => 'int\(\d+\)',
+            self::T_BOOL => 'bool\((?:true|false)\)',
+            self::T_FLOAT => 'float\(\d+\.\d+\)',
+            self::T_NULL => 'NULL',
+            self::T_UNINITIALIZED => 'uninitialized\([\w\\\\]+\)',
+            self::T_ARRAY => 'array\(\d+\)',
+            self::T_ARROW => '=>',
+            self::T_STRING_KEY => '\[\"[a-zA-Z0-9_]+\"\]',
+            self::T_PROTECTED_PROP => '\["\w+":protected\]',
+            self::T_PRIVATE_PROP => '\["\w+":"[\w\\\\]+":private\]',
+            self::T_INT_KEY => '\[\d+\]',
+            self::T_OPEN_BRACE => '\{',
+            self::T_CLOSE_BRACE => '\}',
+            self::T_STD_OBJECT => 'object\(stdClass\)#\d+ \(\d+\)',
+            self::T_RESOURCE => 'resource\(\d+\) of type \(\w+\)',
+            self::T_OBJECT => 'object\([\w\\\\]+\)#\d+ \(\d+\)',
+            self::T_NEWLINE => '\\r?+\\n',
+            self::T_WS => '[ \t]++',
         ];
 
-        $offset = 0;
+        foreach ($patterns as $type => &$pattern) {
+            $pattern = '(?:' . $pattern . ')(*MARK:' . $type . ')';
+        }
+
+        $regexp = '~' . implode('|', $patterns) . '~Asui';
+
+        $res = preg_match_all($regexp, $input, $matches, PREG_SET_ORDER);
+
+        $tokens = [];
+        $inputLen = mb_strlen($input);
         $line = 1;
         $column = 1;
-        $inClass = false;
-        $inClassBraces = 0;
-        while ($offset < mb_strlen($input)) {
-            $firstChar = mb_substr($input, $offset, 1);
-            if ($firstChar === self::SPACE_CHAR) {
-                $offset++;
-                $column++;
-                continue;
-            }
-            if ($firstChar === self::NEW_LINE_CHAR) {
-                $offset++;
-                $column = 1;
+        $tokensLen = 0;
+        foreach ($matches as $match) {
+            $type = $match['MARK'];
+            $tokens[] = new Token($type, $match[0], $line, $column);
+            $tokensLen += mb_strlen($match[0]);
+            if($type === self::T_NEWLINE) {
                 $line++;
+                $column = 1;
                 continue;
             }
-            if ($inClass) {
-                if ($firstChar !== self::OPEN_BRACE_CHAR) {
-                    throw new LexerException(mb_substr($input, $offset), $line, $column);
-                }
-                $inClassBraces++;
+            if ($type === self::T_STRING) {
+                $textLines = explode("\n", $match[0]);
+                $line += count($textLines) - 1;
+                $column += mb_strlen(end($textLines));
                 continue;
-
             }
-
-            $matched = false;
-            $current = mb_substr($input, $offset);
-            foreach ($patterns as $type => $pattern) {
-                if (preg_match($pattern, $current, $matches) === 1) {
-                    $valueLength = mb_strlen($matches[0]);
-                    $tokens[] = new Token($type, $matches[0], $line, $column);
-                    $offset += $valueLength;
-                    $column += $valueLength;
-                    $matched = true;
-                    break;
-                }
-            }
-
-            if (!$matched) {
-                throw new LexerException($current, $line, $column);
-            }
-
-            if ($type === self::T_CLASS) {
-                $inClass = true;
-            }
+            $column += mb_strlen($match[0]);
         }
+
+        if ($inputLen !== $tokensLen) {
+            // @TODO throw exception here
+        }
+
 
         return $tokens;
     }
