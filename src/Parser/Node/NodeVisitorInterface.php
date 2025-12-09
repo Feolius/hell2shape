b342937192d9f6c0deeb25e67a6fd8acd4f689b1
ivanstrygin <feolius@gmail.com>
2025-12-09 21:59:31 +0300

    Add return types in visitor interface.

--- src/Parser/Node/NodeVisitorInterface.php
+++ src/Parser/Node/NodeVisitorInterface.php
@@ -5,21 +5,72 @@ namespace App\Parser\Node;
 /**
  * Generic visitor interface for traversing the AST.
  *
- * @template R The return type of visit methods
+ * @template R
  */
 interface NodeVisitorInterface
 {
+    /**
+     * @return R
+     */
     public function visitBool(BoolNode $node): mixed;
+
+    /**
+     * @return R
+     */
     public function visitInt(IntNode $node): mixed;
+
+    /**
+     * @return R
+     */
     public function visitFloat(FloatNode $node): mixed;
+
+    /**
+     * @return R
+     */
     public function visitString(StringNode $node): mixed;
+
+    /**
+     * @return R
+     */
     public function visitNull(NullNode $node): mixed;
+
+    /**
+     * @return R
+     */
     public function visitResource(ResourceNode $node): mixed;
+
+    /**
+     * @return R
+     */
     public function visitObject(ObjectNode $node): mixed;
+
+    /**
+     * @return R
+     */
     public function visitAnonymousObject(AnonymousObjectNode $node): mixed;
+
+    /**
+     * @return R
+     */
     public function visitHashmap(HashmapNode $node): mixed;
+
+    /**
+     * @return R
+     */
     public function visitHashmapItem(HashmapItemNode $node): mixed;
+
+    /**
+     * @return R
+     */
     public function visitStdObject(StdObjectNode $node): mixed;
+
+    /**
+     * @return R
+     */
     public function visitStdObjectItem(StdObjectItemNode $node): mixed;
+
+    /**
+     * @return R
+     */
     public function visitList(ListNode $node): mixed;
 }
