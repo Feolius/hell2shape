480ac2426d38e50e1039544c69393d3b02a041bf
Feolius <1930829+Feolius@users.noreply.github.com>
2026-01-08 13:06:03 +0000

    Release 0.3.0-test4

--- src/Version.php
+++ src/Version.php
@@ -9,7 +9,7 @@ namespace Feolius\Hell2Shape;
  */
 final class Version
 {
-    public const VERSION = '0.3.0-test3';
+    public const VERSION = '0.3.0-test4';
 
-    public const RELEASE_DATE = '2026-01-07';
+    public const RELEASE_DATE = '2026-01-08';
 }
